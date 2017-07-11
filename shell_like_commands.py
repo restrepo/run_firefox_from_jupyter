5446913924cd2db9f0bc3a31fd02e4c415fdf162
Diego Restrepo <restrepo@udea.edu.co>
2017-07-11 07:15:02 -0400

    Full example of patents

---
+++ shell_like_commands.py
@@ -0,0 +1,8 @@
+def grep(pattern,multilinestring):
+    '''Grep replacement in python
+    as in: $ echo $multilinestring | grep pattern
+    dev: re.M is for multiline strings
+    '''
+    import re 
+    grp=re.finditer('(.*)%s(.*)' %pattern, multilinestring,re.M)
+    return '\n'.join([g.group(0) for g in grp])
